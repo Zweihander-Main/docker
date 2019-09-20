8d85818d3f45fdd27243e420d6a825718049e2a9
Zweihänder <zweidev@zweihander.me>
2019-09-20 00:10:16 -0400

    Change cron log level

--- ankisyncd/dockerfiles/cron-dnscrypt-server-docker.sh
+++ ankisyncd/dockerfiles/cron-dnscrypt-server-docker.sh
@@ -4,8 +4,5 @@ set -eu
 /usr/bin/dig . NS @75.127.96.89 > /opt/unbound/etc/unbound/var/opennic.cache
 echo "0 0 1 * * /usr/bin/dig . NS @75.127.96.89 > /opt/unbound/etc/unbound/var/opennic.cache" \
 	> /var/spool/cron/crontabs/_unbound
-sed '5i\ \ root-hints: "/opt/unbound/etc/unbound/var/opennic.cache"' \
-	/opt/unbound/etc/unbound/unbound.conf \
-	> /opt/unbound/etc/unbound/unbound.conf
 
-exec busybox crond -f -l 0 -L /dev/stdout
+exec busybox crond -f -l 8 -L /dev/stdout
