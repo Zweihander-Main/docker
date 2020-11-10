ed9442e2b73cc0505386f190afe6e023abdf3fc9
Zweihänder <zweidev@zweihander.me>
2020-11-10 14:44:03 -0500

    fix(root): clean up for repo splitting

--- ankisyncd/dockerfiles/cron-dnscrypt-server-docker.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-set -eu
-
-/usr/bin/dig . NS @75.127.96.89 > /opt/unbound/etc/unbound/var/opennic.cache
-echo "0 0 1 * * /usr/bin/dig . NS @75.127.96.89 > /opt/unbound/etc/unbound/var/opennic.cache" \
-	> /var/spool/cron/crontabs/_unbound
-
-exec busybox crond -f -l 8 -L /dev/stdout
