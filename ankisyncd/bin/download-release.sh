25f10f3e0577e22af1fe593a05ee5ba727b4a2cf
Zweihänder <zweidev@zweihander.me>
2022-06-06 16:56:56 -0400

    feat(root): initial commit with working healthcheck

---
+++ ankisyncd/bin/download-release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# file: download-release.sh
+
+mkdir -p release
+
+cd release
+
+git clone https://github.com/ankicommunity/anki-sync-server --branch v2.3.0
+
+mv anki-sync-server/src/* .
+
+rm -rf anki-sync-server
+
+cd ..
