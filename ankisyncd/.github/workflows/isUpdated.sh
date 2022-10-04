41b424d293bff668c3372a4ee03eaf1497a37932
Zweihänder <zweidev@zweihander.me>
2022-10-03 22:49:15 -0400

    build(github): remove unneeded workflows

--- ankisyncd/.github/workflows/isUpdated.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exit $(curl -s https://api.github.com/repos/ankicommunity/anki-sync-server/commits | jq -r "((now - (.[0].commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
